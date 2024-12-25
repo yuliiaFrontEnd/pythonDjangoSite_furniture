16fdb4c6fb166b3527979eb16566ff687323b95f
yuliiaFrontEnd <yuliia.shevchenko27@gmail.com>
2024-12-25 14:31:35 +0100

    Create main  app, index and  about pages

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,7 @@ https://docs.app.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.app.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS: list[Path] = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.app.com/en/5.1/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request) -> HttpResponse:
     context: dict[str, str] = {
-        'title': 'Home',
-        'content': 'The main page of the Home',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_autenticated': False
+        'title': 'Home main',
+        'content': "Магазин мебели HOME"
+        
     }
 
     return render(request, 'main/index.html', context)
 
+
 def about(request) -> HttpResponse:
-    return HttpResponse('About page')
+    context: dict[str, str] = {
+        'title': 'Home main',
+        'content': "About us",
+        'text_on_page': "Information about furnitures."
+    }
+
+    return render(request, 'main/about.html', context)
