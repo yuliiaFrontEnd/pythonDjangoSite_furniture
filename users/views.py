d6ac197bf00607e13d9525e1d1205ffc9c0fb354
yuliiaFrontEnd <yuliia.shevchenko27@gmail.com>
2025-01-19 20:40:36 +0100

    Add to cart view and refactoring cart

--- users/views.py
+++ users/views.py
@@ -18,7 +18,8 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{ username }, You are enter")
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user: logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                
                 return HttpResponseRedirect(reverse('main:index'))
